e388425e91b2d8e29112a644681fe28ba0b42609
Vishal Kumar <vishal97shady@gmail.com>
2023-01-21 20:24:57 +0530

    api token changed2

--- client/script.js
+++ client/script.js
@@ -8,11 +8,14 @@ let loadInterval;
 
 function loader(element) {
   element.textContent = "";
+
   loadInterval = setInterval(() => {
+    // Update the text content of the loading indicator
     element.textContent += ".";
 
+    // If the loading indicator has reached three dots, reset it
     if (element.textContent === "....") {
-      element.textContent = ".";
+      element.textContent = "";
     }
   }, 300);
 }
@@ -30,6 +33,9 @@ function typeText(element, text) {
   }, 20);
 }
 
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
 function generateUniqueId() {
   const timestamp = Date.now();
   const randomNumber = Math.random();
@@ -59,24 +65,26 @@ const handleSubmit = async (e) => {
 
   const data = new FormData(form);
 
+  // user's chatstripe
   chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
 
+  // to clear the textarea input
   form.reset();
 
-  //bot's chatstripe
-
+  // bot's chatstripe
   const uniqueId = generateUniqueId();
   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
+  // to focus scroll to the bottom
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
+  // specific message div
   const messageDiv = document.getElementById(uniqueId);
 
+  // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  //fetch data from server -> bot's response
-
-  const response = await fetch("https://aisolution.onrender.com", {
+  const response = await fetch("https://codex-im0y.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -92,8 +100,6 @@ const handleSubmit = async (e) => {
   if (response.ok) {
     const data = await response.json();
     const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
-
-    // console.log({ parsedData });
 
     typeText(messageDiv, parsedData);
   } else {
